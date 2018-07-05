801eeaf55f0f89e2421bed9fe5b66b86e2eb74f5
Nadya Rizky Ayyub Tam <nadya@stiki.ac.id>
2018-07-05 17:07:34 +0700

    Membuat class yang berisi function untuk menghitung jarak, mengurutkan jarak, mendapatkan lokasi.

--- knn.php
+++ knn.php
@@ -7,14 +7,44 @@ class KNN
 {
 	private $dataSet;
 	private $location;
-	private $dataBaru;
-	private $distance;
+	private $newData;
+	private $distance = array();
 
 	function __construct($data,$loc)
 	{
 		$this->dataSet= $data;
 
-		$this->localtion= $loc;
+		$this->location= $loc;
+	}
+
+	public function getDistance(){
+		sort($this->distance);
+		return $this->distance;
+	}
+
+	public function getSortDistance(){
+		return asort($this->distance);
+	}
+
+	public function getLoc(){
+		return $this->location;
+	}
+
+
+	public function setNewData($data){
+		$this->newData = $data;
+
+	}
+
+	public function eucladianDistance(){
+		for ($i=0; $i <count($this->dataSet) ; $i++) { 
+			$dis = 0;
+			for ($j=0; $j <count($this->dataSet[0]) ; $j++) { 
+				$dis += pow($this->dataSet[$i][$j]-$this->newData[$j],2);
+			}
+			array_push($this->distance, array(sqrt($dis),$this->location[$i]));
+		
+		}
 	}
 
 
